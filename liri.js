63434a729faaf95516b231701a47b217212c43b6
Morgan M <morgan.maxwell20@gmail.com>
2019-01-19 16:12:30 -0700

    just need to do file appending now

--- liri.js
+++ liri.js
@@ -18,7 +18,7 @@ var liriInput = process.argv.slice(3).join(" ");
 // switch statement to process userInput
 switch (liriCommand) {
     case "spotify-this-song":
-        Spotify();
+        spotifyThis();
         break;
     case "concert-this":
         bit();
@@ -32,21 +32,34 @@ switch (liriCommand) {
 };
 
 // music
-function Spotify() {
-    spotify.search({ type: 'track', query: 'All the Small Things' }, function (err, data) {
+function spotifyThis() {
+    spotify.search({ type: 'track', query: liriInput, limit: 1 }, function (err, data) {
+        var response = data.tracks.items[0];
         if (err) {
             return console.log('Error occurred: ' + err);
         }
-
-        console.log(data);
+        // console.log(response.album.artists.name)
+        console.log([
+            "\nArtist(s): " + response.album.artists[0].name,
+            "Song Name: " + response.name,
+            "Album: " + response.album.name,
+            "Song Preview: " + response.preview_url,
+        ].join("\n"));
     });
 };
 // bit = Bands In Town
 function bit() {
-    axios.get(//todo)
-    ).then(function (response) {
-        console.log()//todo
-    });
+    var url = "https://rest.bandsintown.com/artists/" + liriInput + "/events?app_id=" + keys.bandsInTown.key
+    axios.get(url)
+        .then(function (response) {
+            var data = response.data[0];
+            var date = response.data.datetime
+            console.log([
+                "Venue Name: " + data.venue.name,
+                "Venue City: " + data.venue.city + ", " + data.venue.region,
+                "Date: " + moment(date).format("d/MM/YY h a")
+            ].join("\n"));
+        });
 };
 // movies
 function Omdb() {
