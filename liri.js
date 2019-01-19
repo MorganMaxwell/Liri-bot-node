de421d9c097373bfca63d2666f242b80cb8c20f0
Morgan M <morgan.maxwell20@gmail.com>
2019-01-19 16:42:22 -0700

    comments and cleanup

--- liri.js
+++ liri.js
@@ -11,9 +11,10 @@ var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 var omdb = keys.omdb.key;
 var bandsInTown = keys.bandsInTown.key;
-
+// grab user data from command line
 var liriCommand = process.argv[2];
 var liriInput = process.argv.slice(3).join(" ");
+// variable to go through appendFile motions if needed
 var text = false;
 
 // switch statement to process userInput
@@ -38,6 +39,7 @@ function switchState() {
 function spotifyThis() {
     spotify.search({ type: 'track', query: liriInput, limit: 1 }, function (err, data) {
         var response = data.tracks.items[0];
+        // pretty print the data returned into a variable
         var info = [
             "\nArtist(s): " + response.album.artists[0].name,
             "Song Name: " + response.name,
@@ -48,6 +50,8 @@ function spotifyThis() {
         if (err) {
             return console.log('Error occurred: ' + err);
         }
+        // this is the same for all the functions. If text=true, which means we are using a text file, then run a function.
+        // otherwise, console log it.
         if (text) {
             appendText(info);
         }
@@ -58,17 +62,17 @@ function spotifyThis() {
 };
 // bit = Bands In Town
 function bit() {
-    var url = "https://rest.bandsintown.com/artists/" + liriInput + "/events?app_id=" + keys.bandsInTown.key
+    var url = "https://rest.bandsintown.com/artists/" + liriInput + "/events?app_id=" + bandsInTown
     axios.get(url)
         .then(function (response) {
+            // changing the format of the date and time using moment.js
+            var data = response.data[0];
+            var date = response.data.datetime
             var info = [
                 "Venue Name: " + data.venue.name,
                 "Venue City: " + data.venue.city + ", " + data.venue.region,
                 "Date: " + moment(date).format("d/MM/YY h a")
             ].join("\n");
-
-            var data = response.data[0];
-            var date = response.data.datetime
             if (text) {
                 appendText(info);
             }
@@ -101,6 +105,7 @@ function Omdb() {
 };
 // run the process on a txt file of data
 function txtFile() {
+    // read file to grab user info from it, and store it in variables.
     fs.readFile("random.txt", "utf8", function (err, data) {
         if (err) {
             console.log(error);
@@ -108,11 +113,12 @@ function txtFile() {
         var dataArr = data.split(",");
         liriCommand = dataArr[0];
         liriInput = dataArr[1];
+        // flip the 'switch' to run all the functions as though we've got a txt file of user data.
         text = true;
         switchState();
     });
 };
-
+// append data to txt file.
 function appendText(info) {
     fs.appendFile("random.txt", "\n" + info, function (err) {
         if (err) {
